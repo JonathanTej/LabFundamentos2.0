7bd3dd012ae964258935f3481ae131f79adf2d18
JonathanTej <126288026+JonathanTej@users.noreply.github.com>
2023-03-30 14:44:19 -0600

    Add files via upload

---
+++ Lab#2 Fundamentos de programación/laboratorio#2 parte 2 Fundamentos de programación.py
@@ -0,0 +1,16 @@
+print("numeros enteros")
+
+numero = ("identificar cual es el numero mayor, el numero menor, y en numero de en medio, sin importar el orden en que se introdusca cada numero")
+numuno = int(input("introduce el primer numero: "))
+numdos = int(input("introduce el segundo numero: "))
+numtres = int(input("introduce el tercer numero: "))
+
+print("\n los numeros a identificar son: ",numuno, numdos, "y",numtres, "\n")
+
+
+if numuno == numdos:
+   print("este numero es el mas grande de los tres")
+if numuno == numdos:
+   print("este nuemero es el de en medio de los tres")
+if numuno == numdos:
+   print("este numero es el mas pequeño de los tres")
